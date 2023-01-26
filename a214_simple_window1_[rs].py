39b771f2cc1e46609e6f88aac59d92679e54bd7f
r1khil <rikhil.sharma@outlook.com>
2023-01-26 08:09:47 -0500

    twerk

---
+++ a214_simple_window1_[rs].py
@@ -0,0 +1,9 @@
+#   a214_simple_window1.py
+#   A program creates a window on your screen using Tkinter.
+import tkinter as tk
+
+# main window
+root = tk.Tk()
+root.wm_geometry("200x100")
+
+root.mainloop()
