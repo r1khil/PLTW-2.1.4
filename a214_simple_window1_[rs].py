4ea687f65890eae77a826acca064746ab1fd15ca
r1khil <rikhil.sharma@outlook.com>
2023-01-27 13:03:11 -0500

    twerk

--- a214_simple_window1_[rs].py
+++ a214_simple_window1_[rs].py
@@ -5,5 +5,14 @@ import tkinter as tk
 # main window
 root = tk.Tk()
 root.wm_geometry("200x100")
+root.title('Authorization')
+frame_login = tk.Frame(root)
+
+frame_login.grid()
+
+lbl_username = tk.Label(frame_login, text='Username:')
+lbl_username.pack()
+
+tk.Label(frame_login,text="Password:",font="Courier")
 
 root.mainloop()
